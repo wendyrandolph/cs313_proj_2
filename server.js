bbf13040898f973b4e81f1110d31f539ce335d2a
wabbitslayer <wabbitslayer24@gmail.com>
2021-03-24 12:13:30 -0600

    remove login

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.render('pages/category.ejs')
+  res.render('pages/game.ejs')
 });
 
 app.get('/getAPI', function (req, res, next) {
